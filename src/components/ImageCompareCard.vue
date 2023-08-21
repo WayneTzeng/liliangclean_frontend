2872097fa211b28f7c3345af05175908bef0d014
JosephChen <joseoh_chen@17life.com>
2023-08-21 14:40:41 +0800

    add image compare

--- src/components/ImageCompareCard.vue
+++ src/components/ImageCompareCard.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="image-compare-card" :class="{ 'is-first': isFirst }">
-    <img :src="imgData.image" />
+    <ImgComparisonSlider direction="vertical" tabindex="0" class="rendered">
+      <!-- eslint-disable -->
+      <img slot="first" style="width: 100%" :src="imgData.beforeImage" />
+      <img slot="second" style="width: 100%" :src="imgData.afterImage" />
+      <!-- eslint-enable -->
+    </ImgComparisonSlider>
     <CustomButton class="button" full :text="imgData.btnText" />
   </div>
 </template>
 
 <script>
+import { ImgComparisonSlider } from '@img-comparison-slider/vue'
 import CustomButton from './Button.vue'
 
 export default {
   name: 'ImageCompareCardComponent',
   components: {
+    ImgComparisonSlider,
     CustomButton,
   },
   props: {
