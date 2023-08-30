3d22a10c29c00f289589cf54dcb4025610b8ca62
JosephChen <joseoh_chen@17life.com>
2023-08-30 11:41:33 +0800

    feat: add hooks

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,15 +1,20 @@
-<script lang="ts" setup>
-import { useCounterStore } from '@/store/count'
-import { useServerData } from '@/hooks'
-const counterStore = useCounterStore()
-useServerData(() => {
-  counterStore.increment()
-})
-</script>
-
 <template>
   <div>ABOUT</div>
   <div id="count" @click="counterStore.increment()">
     {{ counterStore.count }}
   </div>
 </template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useCounterStore } from '@/store/count'
+import { useServerData } from '@/hooks'
+const counterStore = useCounterStore()
+useServerData(() => {
+  counterStore.increment()
+})
+onMounted(() => {
+  console.log('onMounted', import.meta.env.SSR)
+})
+console.log('setup', import.meta.env.SSR)
+</script>
